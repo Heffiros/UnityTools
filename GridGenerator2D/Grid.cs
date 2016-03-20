73932b10996feb546c1dcdb5a21f8bd34c141684
unknown <vasax.levy@gmail.com>
2016-03-21 00:45:12 +0100

    Script génération de Map/Grille pour 2D

---
+++ GridGenerator2D/Grid.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Permet de rajouter une case dans le menu unity
+[AddComponentMenu("Custom/Editor")]
+
+public class Grid : MonoBehaviour {
+
+
+    //Paramètre qui gère auteur, largeur et couleur de notre grille
+    public float width = 1.0f;
+    public float height = 1.0f;
+    public Color color = Color.yellow;
+
+    void OnDrawGizmos()
+    {
+        //On récupère la position x y de la caméra
+        Vector3 pos = Camera.current.transform.position;
+        //On initialise la couleur
+        Gizmos.color = this.color;
+        
+        //Premiere boucle
+        for(float y = pos.y - 250.0f; y < pos.y + 250.0f; y+= this.height)
+        {
+            //La fonction Draw Line prend le point de départ et d'arrivé de deux vector
+            Gizmos.DrawLine(
+                    new Vector3(-250.0f, Mathf.Floor(y / this.height) * this.height, 0.0f),
+                    new Vector3(250.0f, Mathf.Floor(y / this.height) * this.height, 0.0f));
+        }
+
+        //Deuxième boucle
+        for (float x = pos.y - 250.0f; x < pos.x + 250.0f; x += this.width)
+        {
+            Gizmos.DrawLine(
+                    new Vector3(Mathf.Floor(x / this.width) * this.width, -250.0f ,  0.0f),
+                    new Vector3(Mathf.Floor(x / this.width) * this.width, 250.0f , 0.0f));
+        }
+
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ GridGenerator2D/GridEditor.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor (typeof(Grid))]
+
+public class GridEditor : Editor {
+
+    Grid grid;
+
+    public void OnEnable()
+    {
+       grid = (Grid)target;
+       SceneView.onSceneGUIDelegate += GridUpdate;
+    }
+
+    public void OnDisable()
+    {
+        SceneView.onSceneGUIDelegate -= GridUpdate;
+    }
+
+    void GridUpdate(SceneView sceneView)
+    {
+        Event e = Event.current;
+        Ray r = Camera.current.ScreenPointToRay(new Vector3(
+                e.mousePosition.x,
+                -e.mousePosition.y + Camera.current.pixelHeight));
+        Vector3 mousePos = r.origin;
+
+        if (e.isKey && e.character == 'a')
+        {
+            GameObject obj;
+            Object prefab = PrefabUtility.GetPrefabParent(Selection.activeObject);
+            obj = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+
+            Vector3 aligned = new Vector3(
+                Mathf.Floor(mousePos.x/grid.width)* grid.width + grid.width/2.0f,
+                Mathf.Floor(mousePos.y/grid.height)*grid.height + grid.height/2.0f,
+                0.0f
+            );
+
+            obj.transform.position = aligned;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
